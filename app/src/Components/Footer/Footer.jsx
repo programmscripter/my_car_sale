8c1e1aac333bec8d2d5830488a92eed930f51114
programmscripter <27msi2010@gmail.com>
2024-04-25 22:17:03 +0400

    Автоматическое обновление

--- app/src/Components/Footer/Footer.jsx
+++ app/src/Components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../../Scss/footer.module.scss';
+import classes from '../../Scss/footer.module.css';
 import { Link } from 'react-router-dom';
 import { footer_menu_1, footer_menu_2, footer_menu_3, footer_menu_4 } from './footer_list';
 import { Icons } from './footer_icons_list';
@@ -11,42 +11,42 @@ const Footer = () => {
   const title_4 = "Dealer";
   const title_5 = "Popular";
   return (
-    <div className={styles.main_block}>
-      <div className={styles.main_block_inner}>
-        <div className={styles.block}>
-          <p className={styles.title}>{title_1}</p>
+    <div className={classes.main_block}>
+      <div className={classes.main_block_inner}>
+        <div className={classes.block}>
+          <p className={classes.title}>{title_1}</p>
           {Icons.map((item, index) => {
             return (
               <Link key={index} to={index == -1 ? "/" : `/${item}`}>{item}</Link>
             )
           })}
         </div>
-        <div className={styles.block}>
-          <p className={styles.title}>{title_2}</p>
+        <div className={classes.block}>
+          <p className={classes.title}>{title_2}</p>
           {footer_menu_1.map((item, index) => {
             return (
               <Link key={index} to={index == -1 ? "/" : `/${item}`}>{item}</Link>
             )
           })}
         </div>
-        <div className={styles.block}>
-          <p className={styles.title}>{title_3}</p>
+        <div className={classes.block}>
+          <p className={classes.title}>{title_3}</p>
           {footer_menu_2.map((item, index) => {
             return (
               <Link key={index} to={index == -1 ? "/" : `/${item}`}>{item}</Link>
             )
           })}
         </div>
-        <div className={styles.block}>
-          <p className={styles.title}>{title_4}</p>
+        <div className={classes.block}>
+          <p className={classes.title}>{title_4}</p>
           {footer_menu_3.map((item, index) => {
             return (
               <Link key={index} to={index == -1 ? "/" : `/${item}`}>{item}</Link>
             )
           })}
         </div>
-        <div className={styles.block}>
-          <p className={styles.title}>{title_5}</p>
+        <div className={classes.block}>
+          <p className={classes.title}>{title_5}</p>
           {footer_menu_4.map((item, index) => {
             return (
               <Link key={index} to={index == -1 ? "/" : `/${item}`}>{item}</Link>
