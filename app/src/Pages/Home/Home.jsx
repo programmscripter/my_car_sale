c5891883c4a9c37a8b81ebaff0b10c3e2c0db5c9
programmscripter <27msi2010@gmail.com>
2024-04-25 13:41:20 +0400

    Автоматическое обновление

--- app/src/Pages/Home/Home.jsx
+++ app/src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from '../../Scss/home.module.scss';
 import Animation from '../../Components/Animation/Animation';
-// import Top_content from '../../Components/Home_contents/Top_content/Top_content';
+import Top_content from '../../Components/Home_contents/Top_content/Top_content';
 
 const Home = () => {
   return (
@@ -9,7 +9,7 @@ const Home = () => {
       <Animation />
       <div className={styles.block_inner}>
         <div className={styles.top_content}>
-          {/* <Top_content /> */}
+          <Top_content />
         </div>
       </div>
     </div>
