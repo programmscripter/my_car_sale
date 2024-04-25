ce4465216dd01598f873fb53e3ccf8ea29698cc7
programmscripter <27msi2010@gmail.com>
2024-04-25 12:40:37 +0400

    Автоматическое обновление

--- app/src/Components/Footer/footer_list.jsx
+++ app/src/Components/Footer/footer_list.jsx
@@ -1,21 +1,25 @@
-export const footer_menu_1 = [
+const footer_menu_1 = [
     'About us', 'Newsroom', 'Careers',
     'Advertising', 'Help', 'Contact',
 ]
 
-export const footer_menu_2 = [
+export default footer_menu_1
+
+const footer_menu_2 = [
     'Imprint', 'General Terms and Conditions', 'Privacy Policy',
     'Privacy Settings'
 ]
 
-export const footer_menu_3 = [
+export default footer_menu_2
+
+const footer_menu_3 = [
     'Login', 'Register', 'Our Offer', 'Price list',
     'Terms and Conditions'
 ]
+
+export default footer_menu_3
 
 const footer_menu_4 = [
     'topçu.az IOS', 'topçu.az Android',
     'automobile.it'
 ]
-
-export default footer_menu_4
