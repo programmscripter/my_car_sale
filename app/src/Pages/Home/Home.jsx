0ba59abb4fc06d97840bebfbc9746604c5a90a23
programmscripter <27msi2010@gmail.com>
2024-04-25 22:29:29 +0400

    Автоматическое обновление

--- app/src/Pages/Home/Home.jsx
+++ app/src/Pages/Home/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import styles from '../../Scss/home.module.scss';
+import classes from '../../Scss/home.module.css';
 import Animation from '../../Components/Animation/Animation';
 import Top_content from '../../Components/Home_contents/Top_content/Top_content';
 
 const Home = () => {
   return (
-    <div className={styles.block}>
+    <div className={classes.block}>
       <Animation />
-      <div className={styles.block_inner}>
-        <div className={styles.top_content}>
+      <div className={classes.block_inner}>
+        <div className={classes.top_content}>
           <Top_content />
         </div>
       </div>
