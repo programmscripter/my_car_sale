23cbe98d2dd7e28b3a3bade6bff6103ccf9cdfe4
programmscripter <27msi2010@gmail.com>
2024-04-25 08:43:54 +0400

    Автоматическое обновление

--- app/src/App.js
+++ app/src/App.js
@@ -13,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/profile page' element={<Profile_page />} />
+
           <Route path='/register page' element={<Register_page />} />
         </Routes>
         <Footer />
