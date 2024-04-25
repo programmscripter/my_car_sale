4b80670293e4ea639bf5c358b982a8801500ecc2
programmscripter <27msi2010@gmail.com>
2024-04-25 13:45:26 +0400

    Автоматическое обновление

--- app/src/Components/Home_contents/Top_content/Top_content.jsx
+++ app/src/Components/Home_contents/Top_content/Top_content.jsx
@@ -3,7 +3,7 @@ import Button from '@mui/material/Button';
 import styles from '../../../Scss/top_content.module.scss';
 
 const Top_content = () => {
-    const txt = "Do you want to continue?";
+    const txt = "Welcome to our site!";
     const btn_txt = "Get started";
     return (
         <div className={styles.block}>
