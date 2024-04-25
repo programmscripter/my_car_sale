45c9a1bd5763ea68411d3a81e11bd006949029e3
programmscripter <27msi2010@gmail.com>
2024-04-25 13:38:15 +0400

    Автоматическое обновление

--- app/src/Components/Home_contents/Popular_brands_content/Popular_brands_content.jsx
+++ app/src/Components/Home_contents/Popular_brands_content/Popular_brands_content.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import styles from '../../../'
+import styles from '../../../Scss/popular_brands_content.module.scss';
 
 const Popular_brands_content = () => {
   return (
-    <div>Popular_brands_content</div>
+    <div className={styles.main_block}>
+        <div className={styles.main_block_inner}>
+            
+        </div>
+    </div>
   )
 }
 
