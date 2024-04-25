49e85b6a3f5ec47cb34cac1f410ca85dfbd6ec53
programmscripter <27msi2010@gmail.com>
2024-04-25 13:20:47 +0400

    Автоматическое обновление

--- app/src/Components/Footer/footer_icons_list.jsx
+++ app/src/Components/Footer/footer_icons_list.jsx
@@ -1,5 +1,9 @@
-import { Facebook_icon } from "../../Svg/svg"
+import { Facebook_icon, Instagram_icon, Linkedin_icon, Twitter_icon, Youtube_icon } from "../../Svg/svg"
 
 export const Icons = [
-    
+    <Facebook_icon />, 
+    <Twitter_icon />,
+    <Youtube_icon />,
+    <Instagram_icon />,
+    <Linkedin_icon />
 ]
