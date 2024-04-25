0daa2526e2811d1846a3b810b9d972bbf37ae76b
programmscripter <27msi2010@gmail.com>
2024-04-25 09:05:31 +0400

    Автоматическое обновление

--- app/src/Components/Footer/footer_list.jsx
+++ app/src/Components/Footer/footer_list.jsx
@@ -4,7 +4,8 @@ export const footer_menu_1 = [
 ]
 
 export const footer_menu_2 = [
-    
+    'Imprint', 'General Terms and Conditions', 'Privacy Policy',
+    'Privacy Settings'
 ]
 
 export const footer_menu_3 = [
