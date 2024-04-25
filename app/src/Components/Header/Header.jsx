974e32186b3d0d37a77c4c50b86e1354e5d549a8
programmscripter <27msi2010@gmail.com>
2024-04-25 22:18:05 +0400

    Автоматическое обновление

--- app/src/Components/Header/Header.jsx
+++ app/src/Components/Header/Header.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import styles from '../../Scss/header.module.scss';
+import classes from '../../Scss/header.module.css';
 import Logotype from './Logotype/Logotype';
 import Menu from './Menu/Menu';
 
 const Header = () => {
   return (
-    <div className={styles.block}>
-        <div className={styles.block_inner}>
-            <div className={styles.left_box}>
+    <div className={classes.block}>
+        <div className={classes.block_inner}>
+            <div className={classes.left_box}>
                 <Logotype />
             </div>
-            <div className={styles.right_box}>
+            <div className={classes.right_box}>
                 <Menu />
             </div>
         </div>
