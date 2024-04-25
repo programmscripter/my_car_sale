1c3926566911ffc726049f73d7e0701eb410a02e
programmscripter <27msi2010@gmail.com>
2024-04-25 08:58:17 +0400

    Автоматическое обновление

--- app/src/Components/Footer/Footer.jsx
+++ app/src/Components/Footer/Footer.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import styles from '../../Scss/footer.module.scss';
 
 const Footer = () => {
+  const title_1 = "Company";
   return (
     <div className={styles.block}>
         <div className={styles.block_inner}>
-            
+            <div className={styles.block}>
+              <p>{title_1}</p>
+              {foo}
+            </div>
         </div>
     </div>
   )
