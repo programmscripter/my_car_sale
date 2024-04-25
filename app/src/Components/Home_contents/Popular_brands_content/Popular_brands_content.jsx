fce0f1b64c87c2a43f12feeb4631c997c4e2cd24
programmscripter <27msi2010@gmail.com>
2024-04-25 13:37:13 +0400

    Автоматическое обновление

---
+++ app/src/Components/Home_contents/Popular_brands_content/Popular_brands_content.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../../../'
+
+const Popular_brands_content = () => {
+  return (
+    <div>Popular_brands_content</div>
+  )
+}
+
+export default Popular_brands_content
