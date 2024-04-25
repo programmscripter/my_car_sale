c04e8c2ecdf14a0579c1310b38b1936d7f4d8ad5
programmscripter <27msi2010@gmail.com>
2024-04-25 13:15:29 +0400

    Автоматическое обновление

--- app/src/Svg/svg.jsx
+++ app/src/Svg/svg.jsx
@@ -4,5 +4,6 @@ import { ReactComponent as Bell_icon } from './header_svg/bell_icon.svg';
 import { ReactComponent as Star_icon } from './header_svg/star_icon.svg';
 import { ReactComponent as User_icon } from './header_svg/user_icon.svg';
 import { ReactComponent as Check_icon } from './header_svg/check_icon.svg';
+import { ReactComponent as Facebook_icon } from './footer_svg/facebook_icon.svg';
 
 export { Menu_icon, Search_icon, Bell_icon, Star_icon, User_icon, Check_icon }
