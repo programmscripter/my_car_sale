6d9a200fe395367b635761cb36b603eca0eac0e9
programmscripter <27msi2010@gmail.com>
2024-04-26 08:40:53 +0400

    Автоматическое обновление

---
+++ app/src/Components/Home_contents/Popular_brands_content/Brands_blocks/Brands_blocks.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Brands_blocks = () => {
+    return (
+        <div className={classes.block}>
+            <div className={classes.img_block}>
+                <img src={img} alt="" />
+            </div>
+            <p></p>
+        </div>
+    )
+}
+
+export default Brands_blocks
