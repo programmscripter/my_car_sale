00e1d982ebe4ee44f6274fdd5a2b938575f9c9ab
programmscripter <27msi2010@gmail.com>
2024-04-26 08:41:55 +0400

    Автоматическое обновление

--- app/src/Components/Home_contents/Popular_brands_content/Brands_blocks/Brands_blocks.jsx
+++ app/src/Components/Home_contents/Popular_brands_content/Brands_blocks/Brands_blocks.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Brands_blocks = () => {
+const Brands_blocks = ({ index, img, title }) => {
     return (
-        <div className={classes.block}>
-            <div className={classes.img_block}>
-                <img src={img} alt="" />
+        <Link to={''}>
+            <div className={classes.block}>
+                <div className={classes.img_block}>
+                    <img src={img} alt="" />
+                </div>
+                <p>{title}</p>
             </div>
-            <p></p>
-        </div>
+        </Link>
     )
 }
 
