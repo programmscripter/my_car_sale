5acfbacd194074497f3982a68340bbc57ee97d0d
programmscripter <27msi2010@gmail.com>
2024-04-26 12:45:14 +0400

    Автоматическое обновление

--- app/src/Components/Home_contents/Popular_brands_content/brands_list.jsx
+++ app/src/Components/Home_contents/Popular_brands_content/brands_list.jsx
@@ -1,14 +1,14 @@
-import Audi_img from '../../../Images/Brands_img/Audi_img.png';
-import Mercedes_Benz_img from '../../../Images/Brands_img/Mercedes_Benz_img.png';
-import BMW_img from '../../../Images/Brands_img/BMW_img.png';
-import Toyota_img from '../../../Images/Brands_img/Toyota_img.png';
-import Renault_img from '../../../Images/Brands_img/Renault_img.png';
-import Ford_img from '../../../Images/Brands_img/Ford_img.png';
-import Volvo_img from '../../../Images/Brands_img/Volvo_img.png';
-import Tesla_img from '../../../Images/Brands_img/Tesla_img.png';
-import Skoda_img from '../../../Images/Brands_img/Skoda_img.png';
-import Volkswagen_img from '../../../Images/Brands_img/Volkswagen_img.png';
-import Hyundai_img from '../../../Images/Brands_img/Hyundai_img.png';
+// import Audi_img from '../../../Images/Brands_img/Audi_img.png';
+// import Mercedes_Benz_img from '../../../Images/Brands_img/Mercedes_Benz_img.png';
+// import BMW_img from '../../../Images/Brands_img/BMW_img.png';
+// import Toyota_img from '../../../Images/Brands_img/Toyota_img.png';
+// import Renault_img from '../../../Images/Brands_img/Renault_img.png';
+// import Ford_img from '../../../Images/Brands_img/Ford_img.png';
+// import Volvo_img from '../../../Images/Brands_img/Volvo_img.png';
+// import Tesla_img from '../../../Images/Brands_img/Tesla_img.png';
+// import Skoda_img from '../../../Images/Brands_img/Skoda_img.png';
+// import Volkswagen_img from '../../../Images/Brands_img/Volkswagen_img.png';
+// import Hyundai_img from '../../../Images/Brands_img/Hyundai_img.png';
 
 
 
--- app/src/Helpers/Pictures.jsx
+++ app/src/Helpers/Pictures.jsx
@@ -1,7 +1,11 @@
 import Logo from '../Images/Logo.jpg';
 
+
 const headerPicturesFile = {
     Logo
 }
 
 export default headerPicturesFile
+
+
+const popular_brands_
