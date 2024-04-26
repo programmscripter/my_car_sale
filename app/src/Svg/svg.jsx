7236d145f33748bd23da71de7193fe447f953e1b
programmscripter <27msi2010@gmail.com>
2024-04-26 10:45:59 +0400

    Автоматическое обновление

--- app/src/Svg/svg.jsx
+++ app/src/Svg/svg.jsx
@@ -9,6 +9,7 @@ import { ReactComponent as Twitter_icon } from './footer_svg/twitter_icon.svg';
 import { ReactComponent as Youtube_icon } from './footer_svg/youtube_icon.svg';
 import { ReactComponent as Instagram_icon } from './footer_svg/instagram_icon.svg';
 import { ReactComponent as Linkedin_icon } from './footer_svg/linkedin_icon.svg';
+import { ReactComponent as Audi_icon } from './brands_svg/audi_icon.svg';
 
 export {
     Menu_icon,
