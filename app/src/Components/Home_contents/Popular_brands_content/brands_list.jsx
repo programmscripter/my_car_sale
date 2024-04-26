f2ea76b4dba638676a9767854d94ebcd46eec48b
programmscripter <27msi2010@gmail.com>
2024-04-26 10:49:05 +0400

    Автоматическое обновление

--- app/src/Components/Home_contents/Popular_brands_content/brands_list.jsx
+++ app/src/Components/Home_contents/Popular_brands_content/brands_list.jsx
@@ -1,18 +1,8 @@
-import Audi_img from '../../../Images/Brands_img/Audi_img.png';
-import Mercedes_Benz_img from '../../../Images/Brands_img/Mercedes_Benz_img.png';
-import BMW_img from '../../../Images/Brands_img/BMW_img.png';
-import Renault_img from '../../../Images/Brands_img/Renault_img.png';
-import Skoda_img from '../../../Images/Brands_img/Skoda_img.png';
-import Toyota_img from '../../../Images/Brands_img/Toyota_img.png';
-import Tesla_img from '../../../Images/Brands_img/Tesla_img.png';
-import Volvo_img from '../../../Images/Brands_img/Volvo_img.png';
-import Volkswagen_img from '../../../Images/Brands_img/Volkswagen_img.png';
-import Ford_img from '../../../Images/Brands_img/Ford_img.png';
-import Opel_img from '../../../Images/Brands_img/Opel_img.png';
+import { Audi_icon } from "../../../Svg/svg";
 
 export const brands_list = [
     {
-        img: Audi_img,
+        img: <Audi_icon />,
         title: 'Audi'
     },
     {
