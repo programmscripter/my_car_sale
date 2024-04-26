7ed8e9b0ed712b120ef0dd31805cbdf14f0725d2
programmscripter <27msi2010@gmail.com>
2024-04-26 12:47:18 +0400

    Автоматическое обновление

--- app/src/Helpers/Pictures.jsx
+++ app/src/Helpers/Pictures.jsx
@@ -1,7 +1,19 @@
 import Logo from '../Images/Logo.jpg';
 
 
-/*
+/* Popular Brands Pictures */
+
+
+import Audi_img from '../Images/Brands_img/Audi_img.png';
+import BMW_img from '../Images/Brands_img/BMW_img.png';
+import Mercedes_Benz_img from '../Images/Brands_img/Mercedes_Benz_img.png';
+import Audi_img from '../Images/Brands_img/Audi_img.png';
+import Audi_img from '../Images/Brands_img/Audi_img.png';
+import Audi_img from '../Images/Brands_img/Audi_img.png';
+import Audi_img from '../Images/Brands_img/Audi_img.png';
+import Audi_img from '../Images/Brands_img/Audi_img.png';
+import Audi_img from '../Images/Brands_img/Audi_img.png';
+import Audi_img from '../Images/Brands_img/Audi_img.png';
 
 
 const headerPicturesFile = {
