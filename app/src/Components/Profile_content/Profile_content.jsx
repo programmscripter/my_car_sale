aac2471cd20ee631012376e5d99c09c6316d8804
programmscripter <27msi2010@gmail.com>
2024-04-26 06:43:24 +0400

    Автоматическое обновление

--- app/src/Components/Profile_content/Profile_content.jsx
+++ app/src/Components/Profile_content/Profile_content.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../../Scss/profile_content.module.scss';
+import classes from '../../Scss/profile_content.module.css';
 import { User_icon } from '../../Svg/svg';
 import { profile_list } from './profile_list';
 import Button from '@mui/material/Button';
@@ -9,13 +9,13 @@ const Profile_content = () => {
     const title = "Cars: ";
     const txt = "You don't have any cars listed, sold or bought yet.";
     return (
-        <div className={styles.block}>
-            <div className={styles.block_inner}>
-                <div className={styles.top_block}>
-                    <div className={styles.img_block}>
-                        <User_icon className={styles.img} />
+        <div className={classes.block}>
+            <div className={classes.block_inner}>
+                <div className={classes.top_block}>
+                    <div className={classes.img_block}>
+                        <User_icon className={classes.img} />
                     </div>
-                    <div className={styles.menu_block}>
+                    <div className={classes.menu_block}>
                         <p>{title}</p>
                         {profile_list.map((item, index) => {
                             return (
@@ -26,7 +26,7 @@ const Profile_content = () => {
                         })}
                     </div>
                 </div>
-                <div className={styles.center_block}>
+                <div className={classes.center_block}>
                     <p>{txt}</p>
                 </div>
             </div>
