c1eabf8c6f9a8bbd56a4c46d9d6e79bd800f6a83
programmscripter <27msi2010@gmail.com>
2024-04-26 07:03:00 +0400

    Автоматическое обновление

--- app/src/Pages/Register_page/Register_page.jsx
+++ app/src/Pages/Register_page/Register_page.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import styles from '../../Scss/register_page.module.scss';
+import classes from '../../Scss/register_page.module.css';
 import Register_content from '../../Components/Register_content/Register_content';
 
 const Register_page = () => {
   return (
-    <div className={styles.block}>
-      <div className={styles.block_inner}>
+    <div className={classes.block}>
+      <div className={classes.block_inner}>
         <Register_content />
       </div>
     </div>
