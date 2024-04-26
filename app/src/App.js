a836669f6c2f6832457303335e4a8e106719061f
programmscripter <27msi2010@gmail.com>
2024-04-26 12:30:51 +0400

    Автоматическое обновление

--- app/src/App.js
+++ app/src/App.js
@@ -6,6 +6,7 @@ import Register_page from './Pages/Register_page/Register_page';
 import Footer from './Components/Footer/Footer';
 import './Css/main.css';
 import Brand_page from './Pages/Brand_page/Brand_page';
+import More_brands_page from './Pages/More_brands_page/More_brands_page';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path='/profile page' element={<Profile_page />} />
           <Route path='/register page' element={<Register_page />} />
           <Route path='/brand/:id' element={<Brand_page />} />
+          <Route path='/more brands' element={<More_brands_page />} />
         </Routes>
         <Footer />
       </Router>
