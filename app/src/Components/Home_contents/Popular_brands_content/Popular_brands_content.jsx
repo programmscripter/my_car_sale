fad7173f460d728e7bec08ccfab431e4b5756669
programmscripter <27msi2010@gmail.com>
2024-04-26 08:25:19 +0400

    Автоматическое обновление

--- app/src/Components/Home_contents/Popular_brands_content/Popular_brands_content.jsx
+++ app/src/Components/Home_contents/Popular_brands_content/Popular_brands_content.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import classes from '../../../Scss/popular_brands_content.module.css';
 
 const Popular_brands_content = () => {
+  const title = "Popular Car Brands";
   return (
     <div className={classes.main_block}>
         <div className={classes.main_block_inner}>
-            <div className=""></div>
+            <div className={classes.grid}>
+              <p>{title}</p>
+              <div className={classes.grid_inner}>
+                
+              </div>
+            </div>
         </div>
     </div>
   )
