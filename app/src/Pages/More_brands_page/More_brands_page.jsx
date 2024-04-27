9c110856468b09cd8054b6b8f518b783bd8edc33
programmscripter <27msi2010@gmail.com>
2024-04-27 09:53:08 +0400

    Автоматическое обновление

--- app/src/Pages/More_brands_page/More_brands_page.jsx
+++ app/src/Pages/More_brands_page/More_brands_page.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import classes from '../../Css/Home_page_content_css/more_brands_page.module.css';
 
 const More_brands_page = () => {
+  const title = "Review";
+  const title_2 = "All brands, series, models and technical data of cars";
   return (
     <div className={classes.main_block}>
       <div className={classes.main_block_inner}>
         <div className={classes.grid}>
+          <div className={classes.title_block}>
+            <p>{title}</p>
+            <p>{title_2}</p>
+          </div>
           <div className={classes.grid_inner}>
-            
           </div>
         </div>
       </div>
