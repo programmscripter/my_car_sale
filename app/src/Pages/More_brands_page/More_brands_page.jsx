7122c068365aed5e810bbfbc17051e5f7223d0d1
programmscripter <27msi2010@gmail.com>
2024-04-27 09:17:16 +0400

    Автоматическое обновление

--- app/src/Pages/More_brands_page/More_brands_page.jsx
+++ app/src/Pages/More_brands_page/More_brands_page.jsx
@@ -3,7 +3,11 @@ import classes from '../../Css/Home_page_content_css/more_brands_page.module.css
 
 const More_brands_page = () => {
   return (
-    <div>More_brands_page</div>
+    <div className={classes.block}>
+      <div className={classes.block_inner}>
+        
+      </div>
+    </div>
   )
 }
 
