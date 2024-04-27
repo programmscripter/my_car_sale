782e6a168e36deeed0840f679c5f13da6b316fe0
programmscripter <27msi2010@gmail.com>
2024-04-27 12:54:00 +0400

    Автоматическое обновление

---
+++ app/src/Components/More_brands_content/Main_content/Brand_blocks/Brand_blocks.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from '../../../../Css/More_brands_page_content_css/brand_blocks.module.css';
+
+const Brand_blocks = () => {
+  return (
+    <div>Brand_blocks</div>
+  )
+}
+
+export default Brand_blocks
