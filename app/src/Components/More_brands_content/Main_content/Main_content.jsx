cfc3992b281b9f1cf8086bdac037ec51aa675396
programmscripter <27msi2010@gmail.com>
2024-04-27 09:58:19 +0400

    Автоматическое обновление

--- app/src/Components/More_brands_content/Main_content/Main_content.jsx
+++ app/src/Components/More_brands_content/Main_content/Main_content.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 const Main_content = () => {
     const title = "Review";
-  const title_2 = "All brands, series, models and technical data of cars";
-  const title_3 = "Top Marken";
-  return (
-    <div className={classes.main_block}>
-    <div className={classes.main_block_inner}>
-      <div className={classes.grid}>
-        <div className={classes.title_block}>
-          <p className={classes.title}>{title}</p>
-          <p className={classes.under_title}>{title_2}</p>
+    const title_2 = "All brands, series, models and technical data of cars";
+    const title_3 = "Top Marken";
+    return (
+        <div className={classes.main_block}>
+            <div className={classes.main_block_inner}>
+                <div className={classes.grid}>
+                    <div className={classes.title_block}>
+                        <p className={classes.title}>{title}</p>
+                        <p className={classes.under_title}>{title_2}</p>
+                    </div>
+                    <p className={classes.second_title}>{title_3}</p>
+                    <div className={classes.grid_inner}>
+
+                    </div>
+                </div>
+            </div>
         </div>
-        <p className={classes.second_title}>{title_3}</p>
-        <div className={classes.grid_inner}>
-          
-        </div>
-      </div>
-    </div>
-  </div>
-  )
+    )
 }
 
 export default Main_content
--- app/src/Pages/More_brands_page/More_brands_page.jsx
+++ app/src/Pages/More_brands_page/More_brands_page.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import classes from '../../Css/Home_page_content_css/more_brands_page.module.css';
+import Main_content from '../../Components/More_brands_content/Main_content/Main_content';
 
 const More_brands_page = () => {
-  const title = "Review";
-  const title_2 = "All brands, series, models and technical data of cars";
-  const title_3 = "Top Marken";
   return (
     <div className={classes.main_block}>
       <div className={classes.main_block_inner}>
-        
+        <div className={classes.main_content}>
+          <Main_content />
+        </div>
       </div>
     </div>
   )
