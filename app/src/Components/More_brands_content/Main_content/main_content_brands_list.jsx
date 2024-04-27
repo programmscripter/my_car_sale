e9052c87f235a32f49b2b08a1eaa72a351afbe8f
programmscripter <27msi2010@gmail.com>
2024-04-27 14:34:19 +0400

    Автоматическое обновление

--- app/src/Components/More_brands_content/Main_content/main_content_brands_list.jsx
+++ app/src/Components/More_brands_content/Main_content/main_content_brands_list.jsx
@@ -1,5 +1,14 @@
+import Audi_img from '../../../Images/Brands_img/Audi_img.png';
 import Mercedes_Benz_img from '../../../Images/Brands_img/Mercedes_Benz_img.png';
-
+import BMW_img from '../../../Images/Brands_img/BMW_img.png';
+import Toyota_img from '../../../Images/Brands_img/Toyota_img.png';
+import Renault_img from '../../../Images/Brands_img/Renault_img.png';
+import Ford_img from '../../../Images/Brands_img/Ford_img.png';
+import Volvo_img from '../../../Images/Brands_img/Volvo_img.png';
+import Tesla_img from '../../../Images/Brands_img/Tesla_img.png';
+import Skoda_img from '../../../Images/Brands_img/Skoda_img.png';
+import Volkswagen_img from '../../../Images/Brands_img/Volkswagen_img.png';
+import Hyundai_img from '../../../Images/Brands_img/Hyundai_img.png';
 
 export const mainContentBrandsList = [
     {
@@ -7,7 +16,7 @@ export const mainContentBrandsList = [
         title: "Mercedes-Benz"
     },
     {
-        img: Mercedes_Benz_img,
+        img: Audi_img,
         title: "Mercedes-Benz"
     },
     {
