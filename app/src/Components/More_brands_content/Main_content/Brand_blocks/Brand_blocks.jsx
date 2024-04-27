7a39b302b8766866f1b65cf9de4840e26fa950f6
programmscripter <27msi2010@gmail.com>
2024-04-27 12:56:05 +0400

    Автоматическое обновление

--- app/src/Components/More_brands_content/Main_content/Brand_blocks/Brand_blocks.jsx
+++ app/src/Components/More_brands_content/Main_content/Brand_blocks/Brand_blocks.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import classes from '../../../../Css/More_brands_page_content_css/brand_blocks.module.css';
 
-const Brand_blocks = () => {
+const Brand_blocks = ({ img, title, under_title, index }) => {
   return (
     <div className={classes.block}>
         <div className={classes.img_block}>
-            
+            <img src={img} alt="" />
+        </div>
+        <div className={classes.title_block}>
+            <p>{title}</p>
+            <p>{under_title}</p>
         </div>
     </div>
   )
