1edaa06334982138b24478c6d306e0896b1f6ff7
programmscripter <27msi2010@gmail.com>
2024-04-27 09:51:04 +0400

    Автоматическое обновление

--- app/src/Pages/More_brands_page/More_brands_page.jsx
+++ app/src/Pages/More_brands_page/More_brands_page.jsx
@@ -3,9 +3,9 @@ import classes from '../../Css/Home_page_content_css/more_brands_page.module.css
 
 const More_brands_page = () => {
   return (
-    <div className={classes.block}>
-      <div className={classes.block_inner}>
-        
+    <div className={classes.main_block}>
+      <div className={classes.main_block_inner}>
+        <div className={classes.}></div>
       </div>
     </div>
   )
