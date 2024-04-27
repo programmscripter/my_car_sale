ff449463e73fccd582c83dbf742349a79f81be47
programmscripter <27msi2010@gmail.com>
2024-04-27 09:57:17 +0400

    Автоматическое обновление

--- app/src/Pages/More_brands_page/More_brands_page.jsx
+++ app/src/Pages/More_brands_page/More_brands_page.jsx
@@ -8,16 +8,7 @@ const More_brands_page = () => {
   return (
     <div className={classes.main_block}>
       <div className={classes.main_block_inner}>
-        <div className={classes.grid}>
-          <div className={classes.title_block}>
-            <p className={classes.title}>{title}</p>
-            <p className={classes.under_title}>{title_2}</p>
-          </div>
-          <p className={classes.second_title}>{title_3}</p>
-          <div className={classes.grid_inner}>
-            
-          </div>
-        </div>
+        
       </div>
     </div>
   )
