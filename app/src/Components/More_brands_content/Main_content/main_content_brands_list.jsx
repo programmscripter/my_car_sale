6dc01835903ed64c770b953efd0b000772be3e28
programmscripter <27msi2010@gmail.com>
2024-04-28 20:47:03 +0400

    Автоматическое обновление

--- app/src/Components/More_brands_content/Main_content/main_content_brands_list.jsx
+++ app/src/Components/More_brands_content/Main_content/main_content_brands_list.jsx
@@ -1,4 +1,3 @@
-// import Audi_img from '../../../Images/Brands_img/Audi_img.png';
 import Audi_img from '../../../Images/Brands_img/Audi_img.png';
 import Mercedes_Benz_img from '../../../Images/Brands_img/Mercedes_Benz_img.png';
 import BMW_img from '../../../Images/Brands_img/BMW_img.png';
