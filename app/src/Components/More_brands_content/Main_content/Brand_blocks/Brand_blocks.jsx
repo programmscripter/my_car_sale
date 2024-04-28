16069d6c1d8b77a752c62809cd3b558bc8c9a1b5
programmscripter <27msi2010@gmail.com>
2024-04-28 16:35:09 +0400

    Автоматическое обновление

--- app/src/Components/More_brands_content/Main_content/Brand_blocks/Brand_blocks.jsx
+++ app/src/Components/More_brands_content/Main_content/Brand_blocks/Brand_blocks.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import classes from '../../../../Css/More_brands_page_content_css/brand_blocks.module.css';
+import { Link } from 'react-router-dom';
 
 const Brand_blocks = ({ img, title, under_title, index }) => {
   return (
-    <div className={classes.block}>
+    <Link to={`/brand/}`}>
+      <div className={classes.block}>
         <div className={classes.img_block}>
-            <img src={img} alt="" />
+          <img src={img} alt="" />
         </div>
         <div className={classes.title_block}>
-            <p>{title}</p>
-            <p>{under_title}</p>
+          <p>{title}</p>
+          <p>{under_title}</p>
         </div>
-    </div>
+      </div>
+    </Link>
   )
 }
 
